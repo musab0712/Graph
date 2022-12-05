7cce21acdafbf8a34aa4df098e342fe8569241c3
Musab Hassan <musabhassan1999@gmail.com>
2022-12-05 15:36:37 +0530

    initial Commit

---
+++ Graphs.java
@@ -0,0 +1,217 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Graphs {
+
+    public static void dfTraversal(int adjMatrix[][], int currVertex, boolean visited[]) {
+        visited[currVertex] = true;
+        System.out.print(currVertex + " ");
+        for(int i = 0; i < adjMatrix.length; i++) {
+            if(adjMatrix[currVertex][i] == 1 && visited[i] == false) {
+                dfTraversal(adjMatrix, i, visited);
+            }
+        }
+    }
+
+    public static void dfTraversal(int adjMatrix[][]) {
+        boolean visited[] = new boolean[adjMatrix.length]; 
+        for(int i = 0; i < adjMatrix.length; i++){
+            if(!visited[i])
+                dfTraversal(adjMatrix, i, visited);
+        }
+    }
+
+    public static void bfTraversal(int adjMatrix[][]) {
+        Queue<Integer> pendingVertex = new  LinkedList<>();
+        boolean visited[] = new boolean[adjMatrix.length];
+        for(int i = 0; i < adjMatrix.length; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                pendingVertex.add(i);
+        
+                while(!pendingVertex.isEmpty()) {
+                    int currVertex = pendingVertex.poll();
+                    System.out.print(currVertex + " ");
+                    for(int j = currVertex; j < adjMatrix.length; j++) {
+                        if(adjMatrix[currVertex][j] == 1 && !visited[j]) {
+                            pendingVertex.add(j);
+                            visited[j] = true;
+                        }
+                    }
+                } 
+            }
+        }
+    }
+
+    public static boolean isPath(int adjMatrix[][], int currVertex, int endVertex, boolean visited[]) {
+        if(currVertex > (adjMatrix.length-1) || endVertex > (adjMatrix.length-1) ) {
+            return false;
+        }
+        if(adjMatrix[currVertex][endVertex] == 1) {
+            return true;
+        }
+        visited[currVertex] = true;
+        for(int i = 0; i < adjMatrix.length; i++) {
+            if(adjMatrix[currVertex][i] == 1 && visited[i] == false) {
+                if(i == endVertex) {
+                    return true;
+                }else{
+                    return isPath(adjMatrix, i, endVertex, visited);
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean isPath(int adjMatrix[][], int startVertex, int endVertex){
+        boolean visited[] = new boolean[adjMatrix.length];
+        return isPath(adjMatrix, startVertex, endVertex, visited);
+    }
+
+    public static ArrayList<Integer> getPath(int adjMatrix[][], int sv, int ev, boolean[] visited) {
+        if(sv == ev) {
+            ArrayList<Integer> ans = new ArrayList<>();
+            ans.add(ev);
+            return ans;
+        }
+        visited[sv] = true;
+        for(int i = 0; i < adjMatrix.length; i++) {
+            if(adjMatrix[sv][i] == 1 && !visited[i]) {
+                ArrayList<Integer> ans = getPath(adjMatrix, i, ev, visited);
+                if(ans != null) {
+                    ans.add(sv);
+                    return ans;
+                }
+            }
+        }
+        return null;
+    } 
+
+    public static ArrayList<Integer> getPath(int adjMatrix[][], int sv, int ev) {
+        boolean visited[] = new boolean[adjMatrix.length];
+        return getPath(adjMatrix, sv, ev, visited);
+    }
+
+    public static void isConnected(int adjMatrix[][], int currVertex, boolean visited[]) {
+        visited[currVertex] = true;
+        for(int i = 0; i < adjMatrix.length; i++) {
+            if(adjMatrix[currVertex][i] == 1 && visited[i] == false) {
+                isConnected(adjMatrix, i, visited);
+            }
+        }
+    }
+
+    public static boolean isConnected(int adjMatrix[][]) {
+        boolean visited[] = new boolean[adjMatrix.length]; 
+        isConnected(adjMatrix, 0, visited);
+        for(int i = 0; i < adjMatrix.length; i++){
+            if(visited[i] == false){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void bfsFill(char[][] grid, int row, int col, boolean[][] visited) {
+        int dr[] = {1, 0, -1, 0};
+        int dc[] = {0, -1, 0, 1};
+        visited[row][col] = true;
+        Queue<Pair> pending = new LinkedList<Pair>();
+        pending.add(new Pair(row, col));
+        int m = grid.length; 
+        int n = grid[0].length;
+        while(!pending.isEmpty()) {
+            int ro = pending.peek().first;
+            int co = pending.peek().second;
+            pending.remove();
+            
+            for(int i = 0; i < 4; i++){
+                int r = ro + dr[i];
+                int c = co + dc[i];
+                if(r >= 0 && r < m && c >= 0 && c < n && !visited[r][c] && grid[r][c] == '1') {
+                        visited[r][c] = true;
+                        pending.add(new Pair(r, c));
+                    }
+            }
+        }
+    }
+
+    private void dfsFill(char[][] grid,int i, int j){
+        if(i>=0 && j>=0 && i<grid.length && j<grid[0].length && grid[i][j]=='1'){
+            grid[i][j]='0';
+            dfsFill(grid, i + 1, j);
+            dfsFill(grid, i - 1, j);
+            dfsFill(grid, i, j + 1);
+            dfsFill(grid, i, j - 1);
+        }
+    }
+    
+    public int numIslands(char[][] grid) {
+        int m = grid.length; // row
+        int n = grid[0].length;  // coloum
+        boolean[][] visited = new boolean[m][n];
+        int count = 0; // for starting point
+        for(int row = 0; row < m; row++) {
+            for(int col = 0; col < n; col++) {
+                // By DFS
+                if(grid[row][col] == '1' ) {
+                    count++;
+                    dfsFill(grid, row, col);
+                }
+                // By BFS
+                if(grid[row][col] == '1' && !visited[row][col]) {
+                    count++;
+                    bfsFill(grid, row, col, visited);
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void printAdjMatrix(int adjMatrix[][]) {
+        for(int i = 0; i < adjMatrix.length ; i++) {
+            for(int j = 0; j < adjMatrix.length; j++) {
+                System.out.print(adjMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int e = sc.nextInt();
+        int adjMatrix[][] = new int[n][n];
+        for(int i = 0; i < e; i++){
+            int v1 = sc.nextInt();
+            int v2  = sc.nextInt();
+            adjMatrix[v1][v2] = 1;
+            adjMatrix[v2][v1] = 1;
+        }
+        System.out.println("Matrix");
+        printAdjMatrix(adjMatrix);
+        System.out.print("Depth First Traversla : ");
+        dfTraversal(adjMatrix);
+        System.out.println();
+        System.out.print("Breadth First Traversal : ");
+        bfTraversal(adjMatrix);
+        System.out.println();
+        // System.out.println("Enter the start and end vertex for the Path: ");
+        // int sv = sc.nextInt();
+        // int ev = sc.nextInt();
+        // boolean isPath = isPath(adjMatrix, start, end);
+        // ArrayList<Integer> ans = getPath(adjMatrix, sv, ev);
+        // if(ans != null){
+        //     System.out.print("Path b/w " + sv + " and " + ev + " : ");
+        //     for(int i = 0; i < ans.size(); i++) {
+        //         System.out.print(ans.get(i) + "  ");
+        //     }
+        // }
+        // else{
+        //     System.out.println(" Path Not Found");
+        // }
+        System.out.print("Is Connected : " + isConnected(adjMatrix));
+
+    }
+}
